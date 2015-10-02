fee5f7490a3c5ee041c337fcdae89cbe9008b0b9
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-10-02 08:53:05 +0200

    fix: layout and box-shadows

--- src/media.jsx
+++ src/media.jsx
@@ -3,6 +3,21 @@ var _ = require('lodash');
 
 var StyleSheet = require('react-style');
 
+var shadowDepth = '1px';
+
+function shadowHelper(level) {
+    var r = ""
+    switch(level) {
+        case 1: r = '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'; break;
+        case 2: r = '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'; break;
+        case 3: r = '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'; break;
+        case 4: r = '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'; break;
+        case 5: r = '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)'; break;
+        default:r = '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'; break;
+    }
+    return r;
+}
+
 var styles = StyleSheet.create({
     root: {
         media: {
@@ -10,7 +25,7 @@ var styles = StyleSheet.create({
             alignItems: 'flex-start',
             width: '400px',
             margin: '1rem',
-            boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12)',
+            boxShadow: shadowHelper(1),
             image: {
                 maxWidth: '33%'
             },
@@ -32,7 +47,7 @@ function getPicture(it) {
 
 function getObjects(data) {
     return _.map(data, (it, k) => {
-        return <div style={styles.root.media}>
+        return <div style={styles.root.media} key={k}>
         {getPicture(it)}
         <p styles={styles.root.media.body} >
             {it.name}
